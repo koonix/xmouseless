e8c5154b0302b84a999d4e8d4f70bae57518344a
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-03-08 09:09:23 +0330

    customize config.h

--- config.h
+++ config.h
@@ -9,32 +9,30 @@ static const unsigned int default_speed = 500;
 
 /* changes the speed of the mouse pointer */
 static SpeedBinding speed_bindings[] = {
-    /* key             speed */  
-    { XK_Super_L,      3000 },
-    { XK_Alt_L,        1500 },
-    { XK_a,            100  },
-    { XK_Control_L,    10   },
+    /* key             speed */
+    { XK_Shift_L,    2000 },
+    { XK_Control_L,  100  },
 };
 
 /* moves the mouse pointer
  * you can also add any other direction (e.g. diagonals) */
 static MoveBinding move_bindings[] = {
     /* key         x      y */
-    { XK_j,        -1,     0 },
-    { XK_l,         1,     0 },
-    { XK_i,         0,    -1 },
-    { XK_k,         0,     1 },
+    { XK_w,         0,    -1 },
+    { XK_a,        -1,     0 },
+    { XK_s,         0,     1 },
+    { XK_d,         1,     0 },
 };
 
 /* 1: left
  * 2: middle
  * 3: right */
 static ClickBinding click_bindings[] = {
-    /* key         button */  
+    /* key         button */
     { XK_space,    1 },
-    { XK_f,        1 },
-    { XK_d,        2 },
-    { XK_s,        3 },
+    { XK_c,        1 },
+    { XK_m,        2 },
+    { XK_r,        3 },
 };
 
 /* scrolls up, down, left and right
@@ -43,20 +41,28 @@ static ScrollBinding scroll_bindings[] = {
     /* key        x      y */
     { XK_n,        0 ,    25 },
     { XK_p,        0 ,   -25 },
-    { XK_plus,     0 ,    80 },
-    { XK_minus,    0 ,   -80 },
-    { XK_h,        25,    0  },
-    { XK_g,       -25,    0  },
+    { XK_f,        0 ,    80 },
+    { XK_b,        0 ,   -80 },
+    { XK_l,        25,    0  },
+    { XK_h,       -25,    0  },
 };
 
 /* executes shell commands */
 static ShellBinding shell_bindings[] = {
-    /* key         command */  
-    { XK_b,        "wmctrl -a firefox" },
-    { XK_0,        "xdotool mousemove 0 0" },
+    /* key         command */
+    { XK_1,        "xdotool mousemove 0 0" },
+    { XK_2,        "xdotool mousemove --polar 0 9999" },
+    { XK_3,        "xdotool mousemove 9999 0" },
+    { XK_4,        "xdotool mousemove --polar 270 9999" },
+    { XK_5,        "xdotool mousemove --polar 0 0" },
+    { XK_6,        "xdotool mousemove --polar 90 9999" },
+    { XK_7,        "xdotool mousemove 0 9999" },
+    { XK_8,        "xdotool mousemove --polar 180 9999" },
+    { XK_9,        "xdotool mousemove 9999 9999" },
 };
 
 /* exits on key release which allows click and exit with one key */
 static KeySym exit_keys[] = {
-    XK_Escape, XK_q, XK_space
+    XK_Escape,
+    XK_q,
 };
